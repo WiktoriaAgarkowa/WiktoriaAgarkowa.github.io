f26a5e9947e589f380f949570edf84c73bba5ebf
wagarkowa <victoriaagarkova96@gmail.com>
2020-09-16 16:19:44 +0200

    add section Team

--- Landing-Page-Restaurant/js/main.js
+++ Landing-Page-Restaurant/js/main.js
@@ -2,6 +2,8 @@
 
 let btns = document.getElementsByClassName('link');
 let about = document.getElementById('about');
+let menu = document.getElementById('menu');
+let team = document.getElementById('team');
 
 console.log(btns)
 
@@ -11,3 +13,16 @@ function aboutButtonClick() {
  }
 
  btns[0].addEventListener('click', aboutButtonClick);
+
+
+function menuButtonClick() {
+    menu.scrollIntoView({behavior: "smooth"});
+ }
+
+ btns[1].addEventListener('click', menuButtonClick);
+
+ function teamButtonClick() {
+    team.scrollIntoView({behavior: "smooth"});
+ }
+
+ btns[2].addEventListener('click', teamButtonClick);
