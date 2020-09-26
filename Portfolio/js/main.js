a3d6215fff79efb26aaefa4714c4072fc8a3268f
wagarkowa <victoriaagarkova96@gmail.com>
2020-09-26 19:43:33 +0200

    correct code js, scss and add new header

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -32,7 +32,14 @@ const formValidate = (e) => {
 
 contactForm.addEventListener('submit', formValidate);
 
+//---Scroll---//
 
+let about = document.getElementById('about');
+let scrlBtn = document.getElementById('scroll_button');
+
+scrlBtn.addEventListener('click', function() {
+  about.scrollIntoView({blick: 'start', behavior: 'smooth'})
+});
 
 
 //---Project section animation---//
@@ -52,62 +59,45 @@ let d = document.documentElement;
 
 let clientWidth = d.clientWidth;
 
+console.log(header);
 
 
 
-//---Clicked names in full ekran---//
 
-function socialAppDescritpion() {
+//---Clicked names in full screen---//
 
+let socAppTxt = "I made the Social-App as part of the assignment during the course. The application has the ability to register (however, after registration, the data is not saved in the API), it is also possible to login and log out. If you are not logged in, 10 posts of random users are displayed on the main page. When authorizing, posts of users are added to which you are subscribed. <br> <br> <span> Technologies: </ span> <br> HTML, CSS, ReactJS, Styled Components, AJAX";
+
+let lanPageTxt = "Landing-Page project which is the hallmark of a sushi restaurant. On the page you can make a table reservation and contact using the form. Some elements are animated with CSS, and reservation information is sent using API. <br> <br> <span> Technologies: </ span> <br> HTML, CSS, SASS, AJAX";
+
+let weathAppTxt = "The Wether-App application has the ability to register (after registration, the data is not saved in the API), it is also possible to login and log out. If you are not logged in, 10 posts of random users are displayed on the main page. When authorizing, posts of users are added to which you are subscribed. <br> <br> <span> Technologies: </ span> <br> HTML, CSS, ReactJS, Styled Components <br>";
+
+const projectAnimation = (h, indxline, line, heading, classname, name, txt) => {
   for(i=0; i<lines.length; i++) {
     lines[i].removeAttribute('style');
   }
-  header.style.border = '1px solid #7BDCFE';
-  lines[0].style.border = '1px solid #7BDCFE';
-  projectHeading.style.color = '#7BDCFE';
+  header.classList.remove('social_app', 'landing_page');
+  header.classList.add(h);
+  indxline.style.border = line;
+  projectHeading.style.color = heading;
 
-
-  display.classList.remove('landing_page_description','wether_app_description');
-  display.classList.add('clicked');
-  display.classList.add('social_app_description');
-  projectHeading.innerText = 'Social-App'; 
-
-  description.innerHTML = "I made the Social-App as part of the assignment during the course. The application has the ability to register (however, after registration, the data is not saved in the API), it is also possible to login and log out. If you are not logged in, 10 posts of random users are displayed on the main page. When authorizing, posts of users are added to which you are subscribed. <br> <br> <span> Technologies: </ span> <br> HTML, CSS, ReactJS, Styled Components, AJAX"
+  display.removeAttribute('class');
+  display.classList.add ('project_display', 'clicked', classname);
+  projectHeading.innerText = name;
+  description.innerHTML = txt; 
 }
 
-function landingPageDescritpion() {
-
-  for(i=0; i<lines.length; i++) {
-    lines[i].removeAttribute('style');
-  }
-  header.style.border = '1px solid #FFE799';
-  lines[1].style.border = '1px solid #FFE799';
-  projectHeading.style.color = '#FFE799';
-
-  display.classList.remove('social_app_description','wether_app_description');
-
-  display.classList.add('clicked');
-  display.classList.add('landing_page_description');
-  projectHeading.innerText = 'Landing-Page';
-  description.innerHTML = "Landing-Page project which is the hallmark of a sushi restaurant. On the page you can make a table reservation and contact using the form. Some elements are animated with CSS, and reservation information is sent using API. <br> <br> <span> Technologies: </ span> <br> HTML, CSS, SASS, AJAX" 
-}
-
-function wetherAppDescritpion() {
-
-  for(i=0; i<lines.length; i++) {
-    lines[i].removeAttribute('style');
-  }
-  header.style.border = '1px solid #FFB496';
-  lines[2].style.border = '1px solid #FFB496';
-  projectHeading.style.color = '#FFB496';
-
-  display.classList.remove('social_app_description','landing_page_description');
-
-  display.classList.add('clicked');
-  display.classList.add('wether_app_description');
-  projectHeading.innerText = 'Wether-App';
-  description.innerHTML = "The Wether-App application has the ability to register (after registration, the data is not saved in the API), it is also possible to login and log out. If you are not logged in, 10 posts of random users are displayed on the main page. When authorizing, posts of users are added to which you are subscribed. <br> <br> <span> Technologies: </ span> <br> HTML, CSS, ReactJS, Styled Components <br>" 
-}
+projectNames[0].addEventListener('click', function() {
+  projectAnimation (
+   'social_app', lines[0], '1px solid #7BDCFE', '#7BDCFE', 'social_app_description', 'Social-App', socAppTxt 
+  )
+});
+projectNames[1].addEventListener('click', function() {
+  projectAnimation (
+    'landing_page', lines[1], '1px solid #FFE799', '#FFE799', 'landing_page_description', 'Landing-Page', lanPageTxt 
+  )
+});
+ 
 
 let buttonClicked = false;
 
@@ -116,57 +106,14 @@ for (i=0; i<projectNames.length; i++) {
     
     panel.classList.remove('close');
     panel.classList.add('active');
-    // panel.classList.toggle('change_text');
-    // panel.classList.toggle('change_text_twice');
+
     
     buttonClicked = true;
     console.log(buttonClicked);
     
-    // if(buttonClicked=== true) {
-    //   console.log('lalala')
-    // }
-    // return buttonClicked;
   });
 
 }
-
-
-if(clientWidth > 880 ) {
-
-  projectNames[0].addEventListener('click', socialAppDescritpion);
-  projectNames[1].addEventListener('click', landingPageDescritpion);
-  projectNames[2].addEventListener('click', wetherAppDescritpion);  
-} 
-
-// if(width> 880 ) {
-
-//   projectNames[0].addEventListener('click', socialAppDescritpion);
-//   projectNames[1].addEventListener('click', landingPageDescritpion);
-//   projectNames[2].addEventListener('click', wetherAppDescritpion);
-
-//   console.log('szerokość większa niż 870px')
-// } else {
-//   console.log('szerokość mniejsza niż 870px')
-// }
-
-
-// if(buttonClicked === false) {
-
-//   for (let i=0; i<projectNames.length; i++){
-//     projectNames[i].addEventListener('mouseleave', 
-  
-//       function removeColor() {
-//         header.style.border = '1px solid #4A4A4A'
-//         projectHeading.style.color = '#4A4A4A';
-        
-//         for (let i=0; i<lines.length; i++){
-//           lines[i].style.border = '1px solid #4A4A4A'
-//           console.log(buttonClicked)
-//         }
-
-//       });
-//   }
-// } 
 
 
 //---Small-screen settings---//
@@ -177,47 +124,33 @@ if(header.classList.contains('header_projects')) {
   console.log('klasa sprawdzona')
 }
 
-if (clientWidth < 880) {
+let content = document.getElementsByClassName('body_flex_item_small_screen')
 
-    
-  for (i = 0; i < projectNames.length; i++) {
+let names = document.getElementsByClassName('project_name_ss')
 
-    projectNames[i].addEventListener("click", function() {
+for (i = 0; i < projectNames.length; i++) {
 
-      
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
+  names[i].addEventListener("click", function() {
 
-      console.log(panel)
-
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-        
+      for (i=0; i<content.length; i++) {
+        content[i].style.maxHeight = null;
       }
-    });
-  }
+
+      for (i=0; i<projectNames.length; i++) {
+        names[i].style.backgroundColor = "transparent";
+      }
+      
+      this.style.backgroundColor = " #7BDCFE"
+      
+      let panel = this.nextElementSibling;
+      
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.scrollIntoView({block: 'center', behavior: 'smooth'});
+
+  });
 }
 
-//---RWD setting---//
 
-window.addEventListener('resize',function(){
-  // console.log(window.innerWidth);
-  let clientWidth = d.clientWidth;
-  
-  if (clientWidth < 880) {
-    console.log('mały')
-  }
-
-  if(clientWidth > 880) {
-    console.log('duży')
-  }
-
-
-
-});
 
 
 
