ae0e2e7b78cba0a40885bf8c0396c39e9cd29b26
wagarkowa <victoriaagarkova96@gmail.com>
2020-10-16 19:46:27 +0200

    change design

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,50 +1,9 @@
-//---Form validation---//
-
-let contactForm = document.getElementById('contact_form');
-let formFields = document.getElementsByClassName('input_form');
-const error = document.getElementById('error_msg');
-
-const formValidate = (e) => {
-
-  console.log('submit!')
 
 
-  let emptyField = false;
-
-  for (let i=0; i<formFields.length; i++) {
-    if (formFields[i].value === '') {
-      emptyField = false;
-      formFields[i].classList.add('error');
-    } else {
-      emptyField = true;
-      formFields[i].classList.remove('error');
-    }
-  }
-
-  if(emptyField) {
-    console.log('nie ma błęów')
-  } else {
-    e.preventDefault();
-    error.innerText = 'Please fill in the required fields';
-  }
-}
 
 
-contactForm.addEventListener('submit', formValidate);
+let html = document.querySelector('html');
 
-
-//---Scroll---//
-
-let about = document.getElementById('about');
-let scrlBtn = document.getElementById('scroll_button');
-
-scrlBtn.addEventListener('click', function() {
-  about.scrollIntoView({blick: 'start', behavior: 'smooth'})
-});
-
-scrlBtn.addEventListener('touchend', function() {
-  about.scrollIntoView({blick: 'start', behavior: 'smooth'})
-});
 
 
 //---Start animation---//
@@ -56,7 +15,8 @@ let headerFullScreen = document.querySelector('.header_full_screen');
 let btnNext = document.querySelector('.second_btn');
 let sections = document.querySelectorAll('section');
 let body = document.getElementById('body');
-
+let footer = document.querySelector('footer');
+console.log(sections)
 
 
 btnStart.addEventListener('click', function() {
@@ -70,107 +30,138 @@ btnStart.addEventListener('click', function() {
   }, 500);
 })
 
+let text = document.getElementsByClassName('text');
+console.log(text);
+
+let projects = document.getElementById('projects');
+
+
 btnNext.addEventListener('click', function() {
 
   headerFullScreen.style.opacity = '0';
   this.style.opacity = '0';
-  body.style.display = 'block';
+
   setTimeout(() => {
-    headerFullScreen.style.display = 'none';
-    body.style.opacity = '1';
-  }, 500);
+  for(i=0; i<text.length;i++){
+    text[i].style.color = '#131212';
+  }
+  
+  for(i=1; i<sections.length; i++) {
+    sections[i].style.display = 'block';
+  }
+  
+  for (const section of sections) {
+    section.style.opacity = '1'
+  }
+  footer.style.display = "block"
+  }, 300);
 })
 
+//---Project Animation---//
+
+let heading = document.getElementsByClassName('heading_progect');
 
 
-//---Project section animation---//
-
-let projectNames = document.getElementsByClassName('project_name');
-let header = document.querySelector('.header_projects');
-let lines = document.getElementsByClassName('line');
-let projectHeading = document.querySelector('.main_headnig');
-let description = document.querySelector('.description');
-let display = document.querySelector('.project_display');
-let panel = document.querySelector('.panel');
-let descriptionSmallScreen = document.getElementsByClassName('body_flex_item_small_screen');
-let live = document.querySelector('.live');
-let gitHub = document.querySelector('.code');
-
-
-//---full screen---//
-
-let socAppTxt = "I made the Social-App as part of the assignment during the course. The application has the ability to register (however, after registration, the data is not saved in the API), it is also possible to login and log out. If you are not logged in, 10 posts of random users are displayed on the main page. When authorizing, posts of users are added to which you are subscribed. <br> <br> <span> Technologies: </ span> <br> HTML, CSS, ReactJS, Styled Components, AJAX";
-
-let lanPageTxt = "Landing-Page project which is the hallmark of a sushi restaurant. On the page you can make a table reservation and contact using the form. Some elements are animated with CSS, and reservation information is sent using API. <br> <br> <span> Technologies: </ span> <br> HTML, CSS, SASS, AJAX";
-
-
-const projectAnimation = (h, indxline, line, heading, classname, name, txt, link,code) => {
-
-  for(i=0; i<lines.length; i++) {
-    lines[i].removeAttribute('style');
-  }
-  header.classList.remove('social_app', 'landing_page');
-  header.classList.add(h);
-  indxline.style.border = line;
-  projectHeading.style.color = heading;
-
-  display.removeAttribute('class');
-  display.classList.add ('project_display', 'clicked', classname);
-  projectHeading.innerText = name;
-  description.innerHTML = txt; 
-  live.setAttribute('href', link);
-  gitHub.setAttribute('href', code);
-}
-
-projectNames[0].addEventListener('click', function() {
-  projectAnimation (
-   'social_app', lines[0], '1px solid #7BDCFE', '#7BDCFE', 'social_app_description', 'Social-App', socAppTxt, ' ', 'https://github.com/WiktoriaAgarkowa/social-app' 
-  )
-});
-projectNames[1].addEventListener('click', function() {
-  projectAnimation (
-    'landing_page', lines[1], '1px solid #FFE799', '#FFE799', 'landing_page_description', 'Landing-Page', lanPageTxt, 'https://wiktoriaagarkowa.github.io/Landing-Page-Restaurant/index.html', 'https://github.com/WiktoriaAgarkowa/WiktoriaAgarkowa.github.io/tree/master/Landing-Page-Restaurant' 
-  )
-});
+let isScrolling = false;
  
+    window.addEventListener("scroll", throttleScroll, false);
+ 
+    function throttleScroll(e) {
+      if (isScrolling == false) {
+        window.requestAnimationFrame(function() {
+          headingShow(e);
+          isScrolling = false;
+        });
+      }
+      isScrolling = true;
+    }
+ 
+document.addEventListener("DOMContentLoaded", headingShow, false);
 
-for (i=0; i<projectNames.length; i++) {
-  projectNames[i].addEventListener('click', function() {
+
+function headingShow() {
+
+  let sources = document.getElementsByClassName('sources');
+
+
+  if(isPartiallyVisible(sources[1])) {
+
+    html.classList.add('black');
+
+    for(i=0; i<heading.length; i++) {
+      heading[i].classList.add('color_dark');
+    }
     
-    panel.classList.remove('close');
-    panel.classList.add('active');
+    for(i=4; i<text.length;i++){
+      text[i].style.color = '#f3f3f3'
+    }
+
+    for(i=0; i<sources.length; i++) {
+      let link = sources[i].children;
+
+      for (const l of link) {
+        l.style.color = '#f3f3f3'
+      }
+    }
+  } else {
+
+      html.classList.remove('black');
+
+      for(i=0; i<heading.length; i++) {
+        heading[i].classList.remove('color_dark');
+      }
+      for(i=4; i<text.length;i++){
+        text[i].style.color = '#131212'
+      }
+
+      for(i=0; i<sources.length; i++) {
+        let link = sources[i].children;
     
-  });
+        for (const l of link) {
+          l.style.color = '#131212'
+        }
+      }
+  }
+
+  for(i=0; i<heading.length; i++) {
+
+    if(isPartiallyVisible(heading[i])) {
+      heading[i].classList.add('active');
+    } else {
+      heading[i].classList.remove('active');
+    }
+
+  }
 
 }
 
-//---small screen---//
 
-let content = document.getElementsByClassName('body_flex_item_small_screen')
+function isPartiallyVisible(el) {
+  let elementBoundary = el.getBoundingClientRect();
 
-let names = document.getElementsByClassName('project_name_ss')
+  let top = elementBoundary.top;
+  let bottom = elementBoundary.bottom;
+  let height = elementBoundary.height;
 
-for (i = 0; i < projectNames.length; i++) {
-
-  names[i].addEventListener("click", function() {
-
-      for (i=0; i<content.length; i++) {
-        content[i].style.maxHeight = null;
-      }
-
-      for (i=0; i<projectNames.length; i++) {
-        names[i].style.backgroundColor = "transparent";
-      }
-      
-      this.style.backgroundColor = " #7BDCFE"
-      
-      let panel = this.nextElementSibling;
-      
-      panel.style.maxHeight = panel.scrollHeight + "px";
-      panel.scrollIntoView({block: 'center', behavior: 'smooth'});
-
-  });
+  return ((top + height > 0) && (height + window.innerHeight >= bottom));
 }
+
+function isFullyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+
+  return ((top > 0) && (bottom <= window.innerHeight));
+
+}
+
+
+
+
+
+
+
 
 
 
