5c8f1fdb80611372c0f4c65f883c768aabdd91fa
wagarkowa <victoriaagarkova96@gmail.com>
2021-02-20 19:31:52 +0100

    Day 7 Array Cardio Day

--- JavaScript -30 Day Challenge/Array Cardio day 2/main.js
+++ JavaScript -30 Day Challenge/Array Cardio day 2/main.js
@@ -3,14 +3,46 @@ const people = [
     { name: 'Kait', year: 1986 },
     { name: 'Irv', year: 1970 },
     { name: 'Lux', year: 2015 }
-  ];
+];
 
-  const comments = [
+const comments = [
     { text: 'Love this!', id: 523423 },
     { text: 'Super good', id: 823423 },
     { text: 'You are the best', id: 2039842 },
     { text: 'Ramen is my fav food ever', id: 123523 },
     { text: 'Nice Nice Nice!', id: 542328 }
-  ];
+];
 
-  console.log(people)
+// const isAdult = people.some(function(person) {
+//     const currentYear = (new Date()).getFullYear();
+//     if(currentYear - person.year >= 19) {
+//         return true
+//     }
+// })
+
+const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19)
+
+const allAdult = people.every(person => ((new Date()).getFullYear()) - person.year >= 19)
+
+console.log({isAdult})
+console.log({allAdult})
+
+// const comment = comments.find(function(comment) {
+//     if(comment.id === 823423) {
+//         return true
+//     }
+// })
+
+const comment = comments.find(comment => comment.id === 823423)
+
+console.log({comment})
+
+const index = comments.findIndex(comment => comment.id === 823423)
+
+console.log({index})
+
+const newComments = [
+    ...comments.slice(0, index),
+    ...comments.slice(index + 1)
+];
+
