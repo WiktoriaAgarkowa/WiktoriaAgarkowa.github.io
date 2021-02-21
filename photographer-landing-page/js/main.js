91fbce05107b6450f7c088b00821af97b8ad0485
wagarkowa <victoriaagarkova96@gmail.com>
2021-02-21 16:57:21 +0100

    add hide menu for mobile

--- photographer-landing-page/js/main.js
+++ photographer-landing-page/js/main.js
@@ -1,10 +1,17 @@
 let close = document.getElementById('close'); 
 let menu = document.querySelector('.hide_list_menu');
+let menuItems = document.getElementsByClassName('hide_menu_item')
 
+console.log(menuItems)
 const hideMenu = () => {
-    menu.classList.toggle('close')
+    menu.classList.toggle('close');
+    close.classList.toggle('open');
 }
 
 close.addEventListener('click', hideMenu)
+
+for (let item of menuItems) {
+    item.addEventListener('click', hideMenu)
+}
 
 console.log(menu)
