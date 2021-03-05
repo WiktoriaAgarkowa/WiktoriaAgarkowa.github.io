2e33a33fe8b132b7f6cbacb57004af613e11417c
wagarkowa <victoriaagarkova96@gmail.com>
2021-03-05 18:21:34 +0100

    correct code of the mobile menu section

--- photographer-landing-page/js/main.js
+++ photographer-landing-page/js/main.js
@@ -11,9 +11,11 @@ function hideMenu() {
 
 menuContainer.addEventListener('click', hideMenu)
 
-for (let item of menuItems) {
-    item.addEventListener('click', hideMenu)
-}
+// for (let item of menuItems) {
+//     item.addEventListener('click', function(e){
+//         console.log('e.target')
+//     })
+// }
 
 function swipe () {
     let mobileSlider = document.querySelector('.slider')
